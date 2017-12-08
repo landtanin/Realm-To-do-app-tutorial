623661230d2a2f1b8e0c88f984abf5f38814d2da
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-12-08 10:39:24 +0000

    finish

--- RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/ViewController.swift
@@ -7,19 +7,167 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+// MARK: Model
+
+final class TaskList: Object {
+    @objc dynamic var text = ""
+    @objc dynamic var id = ""
+    let items = List<Task>()
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+final class Task: Object {
+    @objc dynamic var text = ""
+    @objc dynamic var completed = false
+}
+
+class ViewController: UITableViewController {
+    
+    var items = List<Task>()
+    var notificationToken: NotificationToken?
+    var realm: Realm!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
+        
+//        items.append(Task(value: ["text": "My First Task"]))
+        setupRealm()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupUI() {
+        title = "My Tasks"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
+        navigationItem.leftBarButtonItem = editButtonItem
     }
+    
+    func setupRealm() {
+        // Log in existing user with username and password
+        let username = "realm-admin"  // <--- Update this
+        let password = ""  // <--- Update this
+        
+        SyncUser.logIn(with: .usernamePassword(username: username, password: password, register: false), server: URL(string: "http://127.0.0.1:9080")!) { user, error in
+            guard let user = user else {
+                fatalError(String(describing: error))
+            }
+            
+            DispatchQueue.main.async {
+                // Open Realm
+                let configuration = Realm.Configuration(
+                    syncConfiguration: SyncConfiguration(user: user, realmURL: URL(string: "realm://127.0.0.1:9080/~/realmtasks")!)
+                )
+                self.realm = try! Realm(configuration: configuration)
+                
+                // Show initial tasks
+                func updateList() {
+                    
+                    if self.realm.objects(TaskList.self).count == 0 {
+                        
+                        let list = TaskList()
+                        list.id = "000001"
+                        list.text = "lista de prueba"
+                        
+                        // Add to the Realm inside a transaction
+                        try! self.realm.write {
+                            self.realm.add(list)
+                        }
+                        
+                    }
+                    
+                    if self.items.realm == nil, let list = self.realm.objects(TaskList.self).first {
+                        self.items = list.items
+                    }
+                    self.tableView.reloadData()
+                }
+                updateList()
+                
+                // Notify us when Realm changes
+                self.notificationToken = self.realm.observe { _,_ in
+                    updateList()
+                }
+            }
+        }
 
+    }
+    
+    deinit {
+        notificationToken?.invalidate()
+    }
+    
+    // MARK: tableView functions
+    
+    // ------ supporting moving and deleting task --------
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        try! items.realm?.write {
+            items.move(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            try! realm.write {
+                let item = items[indexPath.row]
+                realm.delete(item)
+            }
+        }
+    }
+    // ------ supporting moving and deleting task -------- (end)
+    
+    // ------ support toggling the 'completed' state of a task by tapping it --------
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = items[indexPath.row]
+        try! item.realm?.write {
+            item.completed = !item.completed
+            let destinationIndexPath: IndexPath
+            if item.completed {
+                // move cell to bottom
+                destinationIndexPath = IndexPath(row: items.count - 1, section: 0)
+            } else {
+                // move cell just above the first completed item
+                let completedCount = items.filter("completed = true").count
+                destinationIndexPath = IndexPath(row: items.count - completedCount - 1, section: 0)
+            }
+            items.move(from: indexPath.row, to: destinationIndexPath.row)
+        }
+    }
+    // ------ support toggling the 'completed' state of a task by tapping it -------- (end)
+    
+    override func tableView(_ tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        cell.textLabel?.alpha = item.completed ? 0.5 : 1
+        return cell
+    }
+    
+    @objc func add() {
+        let alertController = UIAlertController(title: "New Task", message: "Enter Task Name", preferredStyle: .alert)
+        var alertTextField: UITextField!
+        alertController.addTextField { textField in
+            alertTextField = textField
+            textField.placeholder = "Task Name"
+        }
+        alertController.addAction(UIAlertAction(title: "Add", style: .default) { _ in
+            guard let text = alertTextField.text , !text.isEmpty else { return }
+            
+            let items = self.items
+            try! items.realm?.write {
+                items.insert(Task(value: ["text": text]), at: items.filter("completed = false").count)
+            }
+        })
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
